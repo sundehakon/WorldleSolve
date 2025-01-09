e8efba0b214e24e5e86af59634313df77762892e
sundehakon <142574795+sundehakon@users.noreply.github.com>
2025-01-09 10:23:05 +0100

    Data fetch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React, { useEffect, useState } from 'react';
 import { Typography } from '@mui/material';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`https://teuteuf-dashboard-assets.pages.dev/data/worldle/games/2025/2025-01-09.json`); // Write logic for automatic checking of date
+        setData(response.data);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+  });
+
   return (
     <Typography></Typography>
   );
