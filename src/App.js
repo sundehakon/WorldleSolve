26346206022cf30f58ed2a58e23e165e0acbda59
sundehakon <142574795+sundehakon@users.noreply.github.com>
2025-01-09 12:54:17 +0100

    Implement UI

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import axios from 'axios';
+import PublicIcon from '@mui/icons-material/Public';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [countryCodeData, setCountryCodeData] = useState(null);
+  const [countries, setCountries] = useState([]);
+  const [countryAnswer, setCountryAnswer] = useState(null);
+  const [reveal, setReveal] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`https://teuteuf-dashboard-assets.pages.dev/data/worldle/games/2025/2025-01-09.json`); // Write logic for automatic checking of date
-        setData(response.data);
+        const countryCodeResponse = await axios.get(`https://teuteuf-dashboard-assets.pages.dev/data/worldle/games/2025/2025-01-09.json`); // Write logic for automatic checking of date
+        setCountryCodeData(countryCodeResponse.data);
+
+        const countriesResponse = await axios.get('https://teuteuf-dashboard-assets.pages.dev/data/common/countries.json');
+        setCountries(countriesResponse.data);
       } catch (error) {
         console.error('Error fetching data: ', error);
       }
@@ -17,9 +24,25 @@ function App() {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    if (countryCodeData && countries.length > 0) {
+      const countryCode = countryCodeData.countryCode;
+      const country = countries.find(c => c.code === countryCode);
+      if (country) {
+        setCountryAnswer(country.name)
+      }
+    }
+  }, [countryCodeData, countries]);
+
   return (
-    <Box>
-      <Typography>{data?.countryCode || 'Loading...'}</Typography>
+    <Box sx={{  display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', textAlign: 'center' }}>
+      <Paper sx={{ height: 300, width: 500, display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', gap: 3 }} elevation={3}>
+        <PublicIcon />
+        <Typography variant='h4'>Worldle Answer</Typography>
+        <Typography variant='subtitle1' sx={{ color: 'text.secondary' }}>9.1.2025</Typography>
+        {reveal === false && <Button variant='outlined' onClick={() => setReveal(true)}>Reveal</Button>}
+        {reveal === true && <Typography>{countryAnswer || 'Loading...'}</Typography>}
+      </Paper>
     </Box>
   );
 }
